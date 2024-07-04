642fd9f24a6f3147a19461f4460c662af3dc90bd
Josip Borozan <138515758+Joske0000@users.noreply.github.com>
2024-07-04 16:31:43 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace VjezbaDelegati
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+        }
     }
-  }
 }
