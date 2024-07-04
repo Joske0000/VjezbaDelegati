781a72e4016b90fa9c0f58ef7aa6f44ac816fa32
Josip Borozan <138515758+Joske0000@users.noreply.github.com>
2024-07-04 17:54:20 +0200

    sss

--- Program.cs
+++ Program.cs
@@ -1,9 +1,49 @@
-﻿namespace VjezbaDelegati
+﻿using System;
+
+namespace VjezbaDelegati
 {
     internal class Program
     {
-        public static void Main(string[] args)
+       public class Semafor
         {
+            public delegate void PromjenaStanjaHandler(string stanje);
+            public event PromjenaStanjaHandler PromjenaStanja;
+
+            public void PromijeniStanje(string novoStanje)
+            {
+                PromjenaStanja?.Invoke(novoStanje);
+            }
+        }
+
+        public class Raskrsnica
+        {
+            private Semafor _semafor;
+
+            public Raskrsnica()
+            {
+                _semafor = new Semafor();
+                _semafor.PromjenaStanja += Semafor_PromjenaStanja;
+            }
+
+            private void Semafor_PromjenaStanja(string novoStanje)
+            {
+                Console.WriteLine($"Stanje semafora na raskrsnici je promijenjeno na: {novoStanje}");
+            }
+
+            public void PromijeniStanjeSemafora(string novoStanje)
+            {
+                _semafor.PromijeniStanje(novoStanje);
+            }
+        }
+
+        public class Program1
+        {
+            public static void Main(string[] args)
+            {
+                Raskrsnica raskrsnica = new Raskrsnica();
+                raskrsnica.PromijeniStanjeSemafora("Crveno");
+                raskrsnica.PromijeniStanjeSemafora("Zeleno");
+            }
         }
     }
 }
